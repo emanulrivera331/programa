ca9784986165c45649c818c56cbddd75f6cf8b8b
emanulrivera331 <124111135+emanulrivera331@users.noreply.github.com>
2023-03-01 21:45:18 -0600

    tarea
    proyecto

---
+++ proyecto.java
@@ -0,0 +1,45 @@
+public  class  vehiculo {
+     cadena  privada marca ;
+     patente de cadena  privada ;
+     Color de cadena  privado ;
+    privado  int  litrosBencina ;
+    
+     Vehiculo publico ( String  marca , String  patente , String  color , int  litrosBencina
+    ){
+        esto _ marca = marca ;
+        esto _ patente = patente ;
+        esto _ color = color ;
+        esto _ litrosBencina = litrosBencina ;
+    }
+
+ 
+    
+     Cadena  pública getMarca (){
+        devolver  esto . marca ;
+    }
+    
+    public  void  setMarca ( String  marca ) {
+        esto _ marca = marca ;
+    }
+    
+    public  void  setPatente ( String  pat ){
+        esto _ patente = palmadita ;
+    }
+    public  String  getPatante (){
+        devolver  esto . patente ;
+    }
+    public  void  setColor ( Color de la cadena  ){
+        esto _ color = color ;
+    }
+     Cadena  pública getColor (){
+        devolver  esto . color ;
+    }
+    public  void  setBencina ( int  bencina ){
+        esto _ litros Bencina = bencina ;
+    }
+    público  int  getBencina (){
+        devolver  esto . litros Bencina ;
+    }
+    
+    //Emanuel rivera Méndez cruz
+}   
